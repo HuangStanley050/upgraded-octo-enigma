8120dcd66df7eee5111c65af7448d30733d8a751
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-01 15:00:23 +1100

    start

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# canvas experiment
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/xguardx/pen/wvggyej](https://codepen.io/xguardx/pen/wvggyej).
+
+
---
+++ src/script.js
@@ -0,0 +1,58 @@
+function multiDimensionalUnique(arr) {
+  const uniques = [];
+  const itemsFound = {};
+  for (let i = 0, l = arr.length; i < l; i++) {
+    let stringified = JSON.stringify(arr[i]);
+    if (itemsFound[stringified]) {
+      continue;
+    }
+    uniques.push(arr[i]);
+    itemsFound[stringified] = true;
+  }
+  return uniques;
+}
+function convertArrayToObject(array) {
+  const initialValue = {};
+  let counter = 0;
+  return array.reduce((obj, item) => {
+    //console.log(obj)
+    return {
+      ...obj,
+      [counter++]: item
+    };
+  }, initialValue);
+}
+function generatePixelArray() {
+  const arr = [];
+  for (let i = 0; i < 32768; i++) {
+    if (!arr[i]) arr[i] = [];
+    for (let j = 0; j < 4; j++) {
+      arr[i][j - 1] = Math.floor(Math.random() * 255) + 0;
+      arr[i][j] = 255;
+    }
+  }
+  //console.log(arr)
+  return arr;
+}
+
+function draw(array) {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const imageData = ctx.createImageData(256, 128);
+
+  // imageData.data[0] = 100;
+  for (let i = 0; i < array.length; i++) {
+    imageData.data[i + 0] = array[i + 0];
+    imageData.data[i + 1] = array[i + 1];
+    imageData.data[i + 2] = array[i + 2];
+    imageData.data[i + 3] = 255;
+  }
+
+  //console.log(imageData.data.length);
+  ctx.putImageData(imageData, 0, 0);
+}
+let result = generatePixelArray();
+let unique = multiDimensionalUnique(result);
+let flatResult = unique.flat();
+
+draw(flatResult);
